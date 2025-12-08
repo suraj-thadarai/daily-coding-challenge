e8a09be4a8276150cd5d4e3ff90fcb022a87b1ae
Suraj Thadarai <programmer@MYLINUX.LOVINGLINUX>
2025-12-08 14:36:53 +0545

    question added into thejbelmudText.js file

--- Javascript Challenge/jbelmudText.js
+++ Javascript Challenge/jbelmudText.js
@@ -1,3 +1,20 @@
+/*
+
+Jbelmud Text
+Given a string, return a jumbled version of that string where each word is transformed using the following constraints:
+
+The first and last letters of the words remain in place
+All letters between the first and last letter are sorted alphabetically.
+The input strings will contain no punctuation, and will be entirely lowercase.
+
+Tests
+Waiting:1. jbelmu("hello world") should return "hello wlord".
+Waiting:2. jbelmu("i love jumbled text") should return "i love jbelmud text".
+Waiting:3. jbelmu("freecodecamp is my favorite place to learn to code") should return "faccdeeemorp is my faiortve pacle to laern to cdoe".
+Waiting:4. jbelmu("the quick brown fox jumps over the lazy dog") should return "the qciuk borwn fox jmpus oevr the lazy dog".
+
+*/
+
 function jbelmu(text) {
 
   let finalString=[];
